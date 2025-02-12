9dad6a32e145dd3186ecefc2ded6884116f1e2e9
spidercoderx <suyashjain2202@gmail.com>
2025-02-13 00:08:53 +0530

    initial commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Chai from "./chai"
+
+function App(){
+
+  const username= "chai aur code"
+  return (
+    <>
+    <Chai/>
+    <h1>chai is ready</h1>
+    <h3>chai aur react {username}</h3>
+    </>
+    
+  )
+}
+
+export default App
